9e6931944c42416a47ad24172a2e65fe22d8e193
minho <kr1227kr@flow.team>
2024-07-23 23:44:19 +0900

    #3 외부 앱으로 열기 구현

--- app/src/main/java/com/startup/histour/presentation/util/extensions/ContextExtension.kt
+++ app/src/main/java/com/startup/histour/presentation/util/extensions/ContextExtension.kt
@@ -1,5 +1,6 @@
 package com.startup.histour.presentation.util.extensions
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -14,5 +15,34 @@ fun Context.moveToAppDetailSetting() {
         }
     } catch (e: Exception) {
         Log.e("ContextExtension", "Context.moveToAppDetailSetting() Error")
+    }
+}
+
+fun Context.openInstagramProfile(username: String) {
+    // 인스타그램 딥링크 URI
+    val instagramUri = Uri.parse("http://instagram.com/_u/$username")
+    val intent = Intent(Intent.ACTION_VIEW, instagramUri).apply {
+        setPackage("com.instagram.android")
+    }
+    // 인스타그램 앱이 설치되어 있는지 확인
+    try {
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            // 인스타그램 앱이 설치되어 있지 않은 경우 웹 브라우저로 열기
+            openBrowser("http://instagram.com/$username")
+        }
+    } catch (e: ActivityNotFoundException) {
+        Log.e("LMH", "실행할 인스타 앱을 못 찾음")
+        openBrowser("http://instagram.com/$username")
+    }
+}
+
+fun Context.openBrowser(url: String) {
+    try {
+        val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(webIntent)
+    } catch (e: Exception) {
+        Log.e("LMH", "url 이 유효하지 않음")
     }
 }
