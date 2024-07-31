92b4b34aeb1f6b36a8b4fe051ae39006d205e490
IslandOfDream <rhffldkt0517@naver.com>
2024-07-31 23:00:41 +0900

    #16 히스투어 상단바 구현

---
+++ app/src/main/java/com/startup/histour/presentation/widget/topbar/TopbarPreviewProvider.kt
@@ -0,0 +1,81 @@
+package com.startup.histour.presentation.widget.topbar
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.startup.histour.R
+
+class TopBarPreviewProvider : PreviewParameterProvider<HistourTopBarModel> {
+    override val values = sequenceOf(
+        HistourTopBarModel(
+            leftSectionType = HistourTopBarModel.LeftSectionType.Empty,
+            rightSectionType = HistourTopBarModel.RightSectionType.Empty,
+            titleStyle = HistourTopBarModel.TitleStyle.Text(R.string.title_character),
+        ),
+        HistourTopBarModel(
+            leftSectionType = HistourTopBarModel.LeftSectionType.Icons(
+                leftIcons = listOf(HistourTopBarModel.TopBarIcon.BACK),
+                onClickLeftIcon = {},
+            ),
+            rightSectionType = HistourTopBarModel.RightSectionType.Empty,
+            titleStyle = HistourTopBarModel.TitleStyle.Text(R.string.title_character),
+        ),
+        HistourTopBarModel(
+            leftSectionType = HistourTopBarModel.LeftSectionType.Icons(
+                listOf(HistourTopBarModel.TopBarIcon.BACK),
+                onClickLeftIcon = {},
+            ),
+            rightSectionType = HistourTopBarModel.RightSectionType.Text(
+                stringResId = R.string.title_character,
+                state = HistourTopBarModel.RightSectionType.Text.State.INACTIVE,
+                onClickRightTextArea = {},
+            ),
+            titleStyle = HistourTopBarModel.TitleStyle.Text(R.string.title_character),
+        ),
+        HistourTopBarModel(
+            leftSectionType = HistourTopBarModel.LeftSectionType.Icons(
+                listOf(HistourTopBarModel.TopBarIcon.BACK),
+                onClickLeftIcon = {},
+            ),
+            rightSectionType = HistourTopBarModel.RightSectionType.Text(
+                stringResId = R.string.save,
+                state = HistourTopBarModel.RightSectionType.Text.State.SAVE,
+                onClickRightTextArea = {},
+            ),
+            titleStyle = HistourTopBarModel.TitleStyle.Text(R.string.title_character),
+        ),
+        HistourTopBarModel(
+            leftSectionType = HistourTopBarModel.LeftSectionType.Icons(
+                listOf(HistourTopBarModel.TopBarIcon.BACK),
+                onClickLeftIcon = {},
+            ),
+            rightSectionType = HistourTopBarModel.RightSectionType.Text(
+                stringResId = R.string.finish,
+                state = HistourTopBarModel.RightSectionType.Text.State.FINISH,
+                onClickRightTextArea = {},
+            ),
+            titleStyle = HistourTopBarModel.TitleStyle.Text(R.string.title_character),
+        ),
+        HistourTopBarModel(
+            leftSectionType = HistourTopBarModel.LeftSectionType.Icons(
+                listOf(HistourTopBarModel.TopBarIcon.BACK),
+                onClickLeftIcon = {},
+            ),
+            rightSectionType = HistourTopBarModel.RightSectionType.Text(
+                stringResId = R.string.complete,
+                state = HistourTopBarModel.RightSectionType.Text.State.COMPLETE,
+                onClickRightTextArea = {},
+            ),
+            titleStyle = HistourTopBarModel.TitleStyle.Text(R.string.title_character),
+        ),
+        HistourTopBarModel(
+            leftSectionType = HistourTopBarModel.LeftSectionType.Icons(
+                listOf(HistourTopBarModel.TopBarIcon.BACK),
+                onClickLeftIcon = {},
+            ),
+            rightSectionType = HistourTopBarModel.RightSectionType.Icons(
+                listOf(HistourTopBarModel.TopBarIcon.BACK),
+                onClickRightIcon = {},
+            ),
+            titleStyle = HistourTopBarModel.TitleStyle.TextWithIcon(R.string.title_character, R.drawable.ic_launcher_foreground),
+        ),
+    )
+}
