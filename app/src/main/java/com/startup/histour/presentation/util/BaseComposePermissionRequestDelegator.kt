5cfcc04834bfd324f72058b3963b3dcf2ba1519c
minho <my_alsgh@naver.com>
2024-08-08 14:05:35 +0900

    #37 권한 요청 클래스 개발(컴포즈용)

---
+++ app/src/main/java/com/startup/histour/presentation/util/BaseComposePermissionRequestDelegator.kt
@@ -0,0 +1,31 @@
+package com.startup.histour.presentation.util
+
+import android.annotation.SuppressLint
+import androidx.activity.compose.ManagedActivityResultLauncher
+import com.startup.histour.core.osversion.UsePermissionHelper
+
+abstract class BaseComposePermissionRequestDelegator(
+    protected val doOnGranted: () -> Unit,
+    protected val doOnShouldShowRequestPermissionRationale: (List<String>) -> Unit,
+    protected val doOnNeverAskAgain: (List<String>) -> Unit,
+) {
+    protected val deniedBeforeLaunchedPermission = mutableListOf<String>()
+
+    abstract val requestPermissionType: UsePermissionHelper.Permission
+
+    abstract val permissionLauncher: ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>
+
+    abstract fun shouldShowRequestPermissionRationale(permission: String): Boolean
+
+    @SuppressLint("ComposableNaming")
+    fun requestPermissionLauncher() {
+        val permission = UsePermissionHelper.getTypeOfPermission(requestPermissionType)
+        permission
+            .also { deniedBeforeLaunchedPermission.clear() }
+            .filter { shouldShowRequestPermissionRationale(it) }
+            .apply {
+                deniedBeforeLaunchedPermission.addAll(this)
+            }
+        permissionLauncher.launch(permission)
+    }
+}
